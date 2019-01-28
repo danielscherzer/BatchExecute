8ba7e93087b98aed016f5baea00c157b7cc91b7e
scherzer <scherzer@hs-weingarten.de>
2019-01-28 12:12:54 +0100

    quick commit

--- BatchExecute/App.xaml.cs
+++ BatchExecute/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using AutoUpdateViaGitHubRelease;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace BatchExecute
 {
@@ -7,9 +11,35 @@ namespace BatchExecute
 	/// </summary>
 	public partial class App : Application
 	{
+		public App()
+		{
+			Task.Run(Update);
+		}
+
 		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
 		{
 			MessageBox.Show(e.Exception.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
+
+		async Task Update()
+		{
+			var updater = new Updater();
+			var assembly = Assembly.GetEntryAssembly();
+			var currentVersion = assembly.GetName().Version;
+			var tempDir = Path.GetTempPath();
+			try
+			{
+				var newVersionAvailable = await updater.DownloadNewVersion("danielScherzer", "BatchExecute", currentVersion, tempDir);
+				if (newVersionAvailable)
+				{
+					var destinationDir = Path.GetDirectoryName(assembly.Location);
+					updater.StartUpdate(tempDir, destinationDir);
+					Shutdown();
+				}
+			}
+			catch
+			{
+			}
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Application update via GitHub latest release
+# Batch Execute
 
 [![Build status](https://ci.appveyor.com/api/projects/status/d1eqkbkgom7hd9xu?svg=true)](https://ci.appveyor.com/project/danielscherzer/batchexecute)
 
 ---------------------------------------
 
-This is a tool for Windows to run shell executable files in batch mode.
+This is a Microsoft Windows tool to run shell executable files in batch mode. Often I want to execute a list of CPU or HDD heavy files, like a parity check. If I would start them all at once, CPU and other resources, like HDD access would degrade performance. So I wrote this tool to start them automatically after each other. This tool either waits for one executable file to finish or if enabled it checks for CPU consumption to drop to zero to close it and start the next executable file in the list.
 
 See the [change log](CHANGELOG.md) for changes and road map.
 
